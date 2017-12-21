2c3261c89e26d55890d5608ca4007de4fa56bcca
andybao <wenyu.bao@gmail.com>
2017-12-21 14:46:31 -0500

    lab 10

--- test/lab10.js
+++ test/lab10.js
@@ -5,10 +5,16 @@ window.onload = pageLoad;
 
 function pageLoad() {
 
+  var formHandle = document.forms[0];
+
+  formHandle.onsubmit = processForm;
+
+}
+
+function processForm() {
   var user_name = document.getElementById("inName").value;
   var user_color = document.getElementById("inColor").value;
 
-  //console.log(user_name + " " + user_color);
+  document.cookie = "name=" + user_name + "color:" + user_color;
 
-  document.cookie = "name=" + user_name;
 }
