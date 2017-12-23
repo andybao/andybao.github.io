ba70d6bb9eb614faa3ecd4d2b23db12552b7eb5a
andybao <wenyu.bao@gmail.com>
2017-12-23 14:06:52 -0500

    update

--- test/assign5-ajax.js
+++ test/assign5-ajax.js
@@ -1,6 +1,5 @@
 //#### ASSIGNMENT 5 - STORE HOURS ####
 
-window.onload = initAll;
 
 var tableObj;
 
@@ -14,18 +13,20 @@ function getTextFile() {
   var xhttp = new XMLHttpRequest();
 
   xhttp.onreadystatechange = function() {
-    alert("xxxx");
+
     if (xhttp.readyState === 4) {
       if (xhttp.status === 200) {
         tableObj.innerHTML = xhttp.responseText;
       }
       else {
-      alert("Connection was unsuccessful: " + xhttp.readyState + ", " + xhttp.status);
+        alert("Connection was unsuccessful: " + xhttp.readyState + ", " + xhttp.status);
     }
   }
 
   xhttp.open("GET", "sched1.txt", true);
 
   xhttp.send(null);
-  };
+  }
 }
+
+window.onload = initAll;
