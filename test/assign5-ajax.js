b7488f249812f246503df65b5bff29b3db5bb2bd
andybao <wenyu.bao@gmail.com>
2017-12-23 14:23:26 -0500

    update

--- test/assign5-ajax.js
+++ test/assign5-ajax.js
@@ -7,7 +7,7 @@ var tableObj;
 function initAll() {
   tableObj = document.getElementById("schedTbl");
   //document.getElementById("routeRb1").onclick = getTextFile("sched1.txt");
-  document.getElementById("routeRb2").onclick = getTextFile("sched2.txt");
+  document.getElementById("routeRb2").onclick = getTextFile();
   //document.getElementById("routeRb3").onclick = getTextFile("sched3.txt");
 }
 
@@ -15,7 +15,7 @@ function getTextFile() {
 
   var xhttp = new XMLHttpRequest();
 
-  xhttp.onreadystatechange = function(location) {
+  xhttp.onreadystatechange = function() {
 
     if (xhttp.readyState === 4) {
       if (xhttp.status === 200) {
@@ -26,7 +26,7 @@ function getTextFile() {
       }
     }
   }
-  xhttp.open("GET", location, true);
+  xhttp.open("GET", "sched2.txt", true);
 
   xhttp.send(null);
 }
